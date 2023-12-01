817383c4077b826253558eb962d7c2aae9edc5c7
Wrinkk <115036933+Wrinkk@users.noreply.github.com>
2023-12-01 05:10:47 -0800

    Add files via upload

---
+++ java-start1/src/Variable/ex/VarEx3.java
@@ -0,0 +1,13 @@
+package Variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long num1 = 10000000000L;
+        System.out.println("num1 = " + num1);
+        boolean num2 = true;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+    }
+}
